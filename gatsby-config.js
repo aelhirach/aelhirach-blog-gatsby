30327ff3ba75069ea1fb13cb086a92ec8f51037d
rzouka <abderrazzak@smartsquare.be>
2020-09-07 16:03:49 +0200

    remove robots.txt plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    'gatsby-plugin-robots-txt',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
