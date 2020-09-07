a826ced4f249e738660bdf7c59b1ef68723acddf
rzouka <abderrazzak@smartsquare.be>
2020-09-08 00:07:53 +0200

    disable crawlers on Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 'use strict'
 
 const siteConfig = require("./config")
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://aelhirach.me",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 
 module.exports = {
   siteMetadata: {
@@ -65,7 +74,27 @@ module.exports = {
           }
         }
       },
-    'gatsby-plugin-robots-txt',
+    {
+        resolve: 'gatsby-plugin-robots-txt',
+        options: {
+          resolveEnv: () => NETLIFY_ENV,
+          env: {
+            production: {
+              policy: [{ userAgent: '*' }]
+            },
+            'branch-deploy': {
+              policy: [{ userAgent: '*', disallow: ['/'] }],
+              sitemap: null,
+              host: null
+            },
+            'deploy-preview': {
+              policy: [{ userAgent: '*', disallow: ['/'] }],
+              sitemap: null,
+              host: null
+            }
+          }
+        }
+      },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
