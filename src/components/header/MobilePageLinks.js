595933b1d97ef5d40dd6d86e64774d50b25677cc
aelhirach <abderrazzak@smartsquare.be>
2021-04-26 17:18:04 +0200

    update

--- src/components/header/MobilePageLinks.js
+++ src/components/header/MobilePageLinks.js
@@ -5,11 +5,10 @@ import "./header.css"
 const MobilePages = () => {
     return (
         <div className="mobile-pages-main" >
-            <div className="text-center">
-                <Link to="/"><span className="linkStyle d-inline p-4">Blog Home</span></Link>
-                <Link to="/about"><span className="linkStyle d-inline p-4">About</span></Link>
-                <a href="mailto:elhirach.abderrazzak@gmail.com?subject=Hello from your website"><span className="linkStyle d-inline p-4">Get in touch</span></a>
-
+            <div className="pages-center">
+                <Link to="/"><span className="linkStyle d-inline">Blog Home</span></Link>
+                <Link to="/about"><span className="linkStyle d-inline">About</span></Link>
+                <a href="mailto:elhirach.abderrazzak@gmail.com?subject=Hello from your website"><span className="linkStyle d-inline">Get in touch</span></a>
             </div>
         </div>
     )
