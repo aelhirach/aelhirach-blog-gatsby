364595f3af250498de55477f6131e62196794edc
aelhirach <abderrazzak@smartsquare.be>
2021-06-21 13:55:57 +0200

    seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,147 +1,85 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
+import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
 import { useLocation } from "@reach/router"
+import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ title, description, lang, image: metaImage, meta }) {
+const SEO = ({ title, description, lang, image, article }) => {
   const { pathname } = useLocation()
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            defaultTitle: title
-            titleTemplate
-            author 
-            defaultDescription: description
-            siteUrl
-            defaultImage: image
-            
-          }
-        }
-      }
-    `
-  )
-  
+  const { site } = useStaticQuery(query)
+
   const {
     defaultTitle,
     titleTemplate,
     defaultDescription,
     siteUrl,
     defaultImage,
-    author
+    twitterUsername,
   } = site.siteMetadata
-  
+
   const seo = {
-      title: title || defaultTitle,
-      description: description || defaultDescription,
-      image: metaImage && metaImage.src? `${site.siteMetadata.siteUrl}${metaImage.src}` : null,
-      url: `${siteUrl}${pathname}`,
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image || defaultImage}`,
+    url: `${siteUrl}${pathname}`,
   }
 
   return (
     <Helmet
-      htmlAttributes={{
-        lang,
-      }}
+      htmlAttributes={{ lang }}
       title={seo.title}
       titleTemplate={titleTemplate}
-      meta={[
-        {
-          name: `description`,
-          content: seo.description,
-        },
-        {   
-          name:"image", 
-          content: seo.image 
-        }, 
-        {   
-          property:"og:url", 
-          content: seo.url 
-        }, 
-        
-        {
-          property: `og:title`,
-          content: seo.title,
-        },
-        {
-          property: `og:description`,
-          content: seo.description,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: author,
-        },
-        {
-          name: `twitter:title`,
-          content: seo.title,
-        },
-        {
-          name: `twitter:description`,
-          content: seo.description,
-        },
-      ].concat(
-          metaImage
-            ? [
-                {
-                  property: "og:image",
-                  content: metaImage,
-                },
-                {
-                  property: "og:image:width",
-                  content: metaImage.width,
-                },
-                {
-                  property: "og:image:height",
-                  content: metaImage.height,
-                },
-                {
-                  name: "twitter:card",
-                  content: "summary_large_image",
-                },
-              ]
-            : [
-                {
-                  name: "twitter:card",
-                  content: "summary",
-                },
-              ]
-        ).concat(meta)}
-    />
+    >
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      {seo.url && <meta property="og:url" content={seo.url} />}
+      {(article ? true : null) && <meta property="og:type" content="article" />}
+      {seo.title && <meta property="og:title" content={seo.title} />}
+      {seo.description && (
+        <meta property="og:description" content={seo.description} />
+      )}
+      {seo.image && <meta property="og:image" content={seo.image} />}
+      <meta name="twitter:card" content="summary_large_image" />
+      {twitterUsername && (
+        <meta name="twitter:creator" content={twitterUsername} />
+      )}
+      {seo.title && <meta name="twitter:title" content={seo.title} />}
+      {seo.description && (
+        <meta name="twitter:description" content={seo.description} />
+      )}
+      {seo.image && <meta name="twitter:image" content={seo.image} />}
+    </Helmet>
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
 
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  article: PropTypes.bool,
+}
 
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+  lang: `en`,
+  article: false,
+}
 
-
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        titleTemplate
+        defaultDescription: description
+        siteUrl
+        defaultImage: image
+        twitterUsername
+      }
+    }
+  }
+`
