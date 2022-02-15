0a987fcdc90ae4a57bde35306aa7ce1d353ad88d
EL HIRACH Abderrazzak <elhirach.abderrazzak@gmail.com>
2022-02-15 11:19:51 +0100

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,21 @@ module.exports = {
     labels: siteConfig.labels,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-PW9KJTC1VN", // Google Analytics / GA
+        ],
 
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
